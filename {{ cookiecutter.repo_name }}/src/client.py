18383f872259608d8e3777cd9b73f34d2e517986
Andre Aguiar <ufabc.andre@gmail.com>
2020-03-24 16:20:57 -0300

    Fix: Handling exception in client.py
    Signed-off-by: Andre Aguiar <ufabc.andre@gmail.com>

--- {{ cookiecutter.repo_name }}/src/client.py
+++ {{ cookiecutter.repo_name }}/src/client.py
@@ -21,10 +21,9 @@ class Client:
 		Use the information contained in the params.py file 
 		to connect to the postgreSQL database.
 		"""
-                try:
-		    self.engine = create_engine(f'postgresql+psycopg2://{params.user}:{params.password}@{params.host}/{params.database}')
-		    self.conn = self.engine.connect()
-                except Exception as e:
-                    logger.warning('Verify your credentials')
-                    raise e
-
+		try:
+			self.engine = create_engine(f'postgresql+psycopg2://{params.user}:{params.password}@{params.host}/{params.database}')
+			self.conn = self.engine.connect()
+		except Exception as e:
+			logger.warning('Could not connect to the database on client.py file.')
+			logger.warning('Verify your credentials.')
