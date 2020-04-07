27c09e2218cca8896de8064d363b8bf32a5e84e8
Andre Aguiar <ufabc.andre@gmail.com>
2020-04-07 18:52:26 -0300

    Fix issue - logging should be defined right after parameter creation
    Signed-off-by: Andre Aguiar <ufabc.andre@gmail.com>

--- {{ cookiecutter.repo_name }}/src/pipeline.py
+++ {{ cookiecutter.repo_name }}/src/pipeline.py
@@ -55,11 +55,11 @@ def process(client, params):
 if __name__ == '__main__': 
 
     params = Params()
-    client = Client(params)
 
     logging.basicConfig(filename=params.log_name,
-                        level=logging.INFO,
-                        format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')
+                    level=logging.INFO,
+                    format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
 
+    client = Client(params)
     process(client, params)
